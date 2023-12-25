38c95019f3b2badb01c3ea2ef7be0ba797ea616e
nawafilhusnul <nawafilhusnul@gmail.com>
2023-12-25 18:51:27 +0700

    fix: hide the env variables

--- utils/mysql/config.go
+++ utils/mysql/config.go
@@ -1,14 +1,23 @@
 package utils
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func GormClient() *gorm.DB {
-	dsn := "user:password@tcp(127.0.0.1:3306)/invoice?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DB"),
+	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to mySQL using gorm: %v", err)
